80ed1f1e1e23455437fbe9b39f971366c50b6b79
marcilio0710 <marcilionarciso@hotmail.com>
2025-07-24 00:25:15 -0300

    Update db.php

--- db.php
+++ db.php
@@ -1,22 +1,16 @@
 <?php
-// Certifique-se de pegar as variáveis corretamente:
 $host = getenv('DB_HOST');
 $port = getenv('DB_PORT');
 $dbname = getenv('DB_NAME');
 $user = getenv('DB_USER');
 $password = getenv('DB_PASS');
-
-// ATENÇÃO para ponto e vírgula antes de sslmode
 $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require";
-//                                ^ aqui está o ponto e vírgula!
 
 try {
     $pdo = new PDO($dsn, $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Conexão bem-sucedida!";
+    // NÃO coloque NADA aqui!!!
 } catch (PDOException $e) {
-    echo "Erro na conexão com o banco: " . $e->getMessage();
-    exit();
+    // A saída de erro abaixo só deve ser usada para debug! O ideal seria tratar melhor em produção.
+    die("Erro na conexão com o banco: " . $e->getMessage());
 }
-
-?>
