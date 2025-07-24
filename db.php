9cbabbadf9fe8e1191c600fdd41abc6fd26a69ff
marcilio0710 <marcilionarciso@hotmail.com>
2025-07-23 21:46:52 -0300

    Update db.php

--- db.php
+++ db.php
@@ -1,4 +1,3 @@
-<?php
 $host = getenv('DB_HOST');
 $port = getenv('DB_PORT');
 $dbname = getenv('DB_NAME');
@@ -9,7 +8,6 @@ try {
     $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    echo "Erro na conexão: " . $e->getMessage();
+    echo "Erro na conexão com o banco: " . $e->getMessage();
     exit();
 }
-?>
